51fb811bc1114942636df58e471c828b2a84bfb6
Kevin Beijer <kevinbeijer1996@gmail.com>
2019-07-10 14:39:33 +0200

    Base movement on terrain

---
+++ NativeAmericanRPG/Assets/Scripts/Movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    [SerializeField] private float speed = 100;
+    [SerializeField] private float jumpHeight = 10;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetButton("Vertical"))
+        {
+            Debug.Log("vert");
+            rb.AddForce(Vector3.forward * Input.GetAxis("Vertical") * speed, ForceMode.Acceleration);
+        }
+        if (Input.GetButton("Horizontal"))
+        {
+            Debug.Log("hor");
+            rb.AddForce(Vector3.right * Input.GetAxis("Horizontal") * speed, ForceMode.Acceleration);
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("jump");
+            rb.AddForce(Vector3.up * jumpHeight * speed, ForceMode.Acceleration);
+        }
+    }
+}
---
+++ NativeAmericanRPG/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController controller;
+
+    [SerializeField] private float jumpHeight = 10;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Vertical"))
+        {
+            Debug.Log("vert");
+            controller.Move(Vector3.forward * Input.GetAxis("Vertical"));
+        }
+        if (Input.GetButton("Horizontal"))
+        {
+            Debug.Log("hor");
+            controller.Move(Vector3.right * Input.GetAxis("Horizontal"));
+        }
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            Debug.Log("jump");
+            controller.Move(Vector3.up * jumpHeight);
+        }
+        if (!controller.isGrounded)
+        {
+            Debug.Log("fall");
+            controller.Move(-Vector3.up);
+        }
+    }
+}
